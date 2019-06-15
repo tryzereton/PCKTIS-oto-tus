8c287bbb74f0a5f30ae58b2046911be45bb372ef
Ferhat <36997714+tryzereton@users.noreply.github.com>
2019-06-15 21:30:19 +0300

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PCKTIS [Oto Tuş]")]
+[assembly: AssemblyDescription("PCKTIS Oto Tuşlayıcı")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ferhat KURT")]
+[assembly: AssemblyProduct("PCKTIS [Oto Tuş]")]
+[assembly: AssemblyCopyright("Copyright ©  2019. Ferhat KURT")]
+[assembly: AssemblyTrademark("Ferhat Kurt")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fe8451e2-da13-4f96-a056-2d3ea3c97e90")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
